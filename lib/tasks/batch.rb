f8c46050031e4cf6cb7dfa3b3c59c4140f616144
kento sasamoto <knote0721@gmail.com>
2015-11-22 02:29:41 +0900

    DB作成

--- lib/tasks/batch.rb
+++ lib/tasks/batch.rb
@@ -3,28 +3,30 @@ class Tasks::Batch
     url = 'http://hypernews.2chblog.jp/atom.xml'
     xml = Faraday.get(url).body
     feed = Feedjira::Feed.parse xml
-    puts feed.title
+    site = feed.title
     feed.entries.each do |entry|
-      getContent(entry)
+      getContent(entry, site)
     end
   end
 
-  def self.getContent(entry)
-    title = entry.title
-    created_at = entry.updated
-    summary = entry.summary
+  def self.getContent(item, site)
+    entry = Entry.new
+    entry.site = site
+    entry.title = item.title
+    entry.content_created_at = item.updated
+    entry.description = item.summary
 
     conn = Faraday::Connection.new(:url => 'http://api.diffbot.com/v3/article') do |builder|
       builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
       builder.use FaradayMiddleware::FollowRedirects
       builder.use Faraday::Adapter::NetHttp     # Net/HTTP をアダプターに使う
     end
-    res = conn.get '', {url: entry.url, token: 'ffb1589fc545ecf85b9947b26f758409'}
+    res = conn.get '', {url: item.url, token: 'ffb1589fc545ecf85b9947b26f758409'}
     result = JSON.parse(res.body)
-    text = result['objects'][0]['text']
-    html = result['objects'][0]['html']
-    image = result['objects'][0]['images'][0]['url']
-    puts title
+    entry.text = result['objects'][0]['text']
+    entry.html = result['objects'][0]['html']
+    entry.image = result['objects'][0]['images'][0]['url']
+    entry.save
     begin
     rescue
     end
