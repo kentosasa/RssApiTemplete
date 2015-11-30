0231d18804030d63072c5ae07800673193cf2f6d
kento sasamoto <knote0721@gmail.com>
2015-11-30 16:39:29 +0900

    DB修正

--- db/migrate/20151128140522_create_contents.rb
+++ db/migrate/20151128140522_create_contents.rb
@@ -2,8 +2,8 @@ class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
       t.integer :entry_id
-      t.text :text
-      t.text :html
+      t.text :text, :limit => 16777215
+      t.text :html, :limit => 16777215
 
       t.timestamps null: false
     end
