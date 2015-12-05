0f15db0ad45ba4fa7e059524dea47c63dfaeccc0
kento sasamoto <knote0721@gmail.com>
2015-12-05 19:12:00 +0900

    画像制限解除

--- lib/tasks/batch.rb
+++ lib/tasks/batch.rb
@@ -34,7 +34,7 @@ class Tasks::Batch
       entry.url = item.url
 
       res = fara.get item.url
-      doc  = Readability::Document.new(res.body, :tags => %w[div p img a], :attributes => %w[src href], :remove_empty_nodes => false, :min_image_height => 200, :min_image_width => 200)
+      doc  = Readability::Document.new(res.body, :tags => %w[div p img a], :attributes => %w[src href], :remove_empty_nodes => false)
       nokogiri = Nokogiri::HTML(doc.content.encode("UTF-8"))
       doc.content.encode("UTF-8")
       entry.image = doc.images[0]
