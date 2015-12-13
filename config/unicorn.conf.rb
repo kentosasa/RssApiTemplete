7220b93165396454c68b375c6c3a6e89b2c61be1
kento sasamoto <knote0721@gmail.com>
2015-12-14 03:47:16 +0900

    メッセージの追加

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -3,7 +3,7 @@ $worker  = 2
 $timeout = 30
 $app_dir = "/var/www/nichannel/current"
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$pid     = '/var/www/nichannel/shared/tmp/pids/unicorn.pid'
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # set config
 worker_processes  $worker
@@ -17,7 +17,6 @@ pid $pid
 preload_app true
 # before starting processes
 before_fork do |server, worker|
-  desc 'Remove Pid'
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
