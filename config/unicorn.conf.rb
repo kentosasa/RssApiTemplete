9462d1d533eb3941625c89ae181ccaa96d0553e3
kento sasamoto <knote0721@gmail.com>
2015-12-21 19:07:35 +0900

    error修正

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -22,7 +22,7 @@ before_fork do |server, worker|
   if old_pid != server.pid
     begin
       Process.kill "QUIT", File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
+    rescue
     end
   end
 end
